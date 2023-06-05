094b93428820b0ac6b6f5ff0f4f906c5c4bb2dbf
gitanshu18 <lusifer0418@gmail.com>
2023-06-05 20:05:01 +0530

    update course.js

--- #14 Numbers and Dates/day 58 -/course.js
+++ #14 Numbers and Dates/day 58 -/course.js
@@ -64,7 +64,7 @@ console.log(Math.floor(-23.3)) // -24
 console.log(Math.floor(-23.9)) // -24
 console.log('')
 
-console.log((23).toFixed(0))
-console.log((5).toFixed(3))
+console.log((23).toFixed(3))
+console.log((23.8).toFixed(0))
 console.log((2.3453).toFixed(2))
-console.log((-34.2).toFixed(5))
+console.log((-34.26).toFixed(5))
