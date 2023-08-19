fc19287e289cf806b3357a96923f05c10865a8e0
gitanshu18 <lusifer0418@gmail.com>
2023-08-19 20:34:30 +0530

    challenge 1 completed

--- #16 object oriented programming/oop1/oops.js
+++ #16 object oriented programming/oop1/oops.js
@@ -8,15 +8,21 @@ function Person(firstName, lastName, birthYear){
     this.age = function (){
         return new Date().getFullYear() - this.birthYear
     }
-}
+};
 
 const gitanshu = new Person('Gitanshu', 'Sankhla', 2004);
+
+// adding new properties or mrthods to function prototype
 Person.prototype.description = function(){
     return `${this.firstName} ${this.lastName} is ${this.age()} years old.`
-}
+};
 
-Person.prototype.species = "Homosapiens"
+Person.prototype.species = "Homosapiens";
 
 console.log(gitanshu.description());
-// console.log(gitanshu.__proto__ === Person.prototype);
-console.log(Person.prototype)
+
+console.log(gitanshu.__proto__ === Person.prototype); // return true
+console.log(Person.prototype);
+
+const arr = [1, 2,3, 4, 5, 6,5];
+console.dir(Array.prototype)
