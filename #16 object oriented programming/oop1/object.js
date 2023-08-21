a399245c1410a5db1f18aa5b4e2b089787e49694
gitanshu18 <lusifer0418@gmail.com>
2023-08-21 21:34:00 +0530

    Understanding Object.create()

--- #16 object oriented programming/oop1/object.js
+++ #16 object oriented programming/oop1/object.js
@@ -1,3 +1,24 @@
-const Person = {
-    
+
+// This Product object can be used as prototype 
+const Product = {
+    company: 'Raymond Gucci',
+    product_type: 'jacket',
+    designer: 'Paterick Bateman'
 }
+
+// instance of Product Object
+const redHoody = Object.create(Product)
+redHoody.color = 'red';
+redHoody.sizes = ['sm', 'md', 'xl', 'xxl'];
+redHoody.product_number = 65774;
+
+const blueHoody = Object.create(Product)
+blueHoody.color = 'blue';
+blueHoody.sizes = ['xl', 'xxl', 'xxxl'];
+blueHoody.product_number = 10989;
+
+console.log(redHoody)
+console.log(blueHoody)
+
+// proto of these instances is the Product Object itself
+console.log(redHoody.__proto__ === Product) // true
