c0adf80e0e6659c39dbf0d49b0b835076713bd0d
gitanshu18 <lusifer0418@gmail.com>
2023-08-24 17:36:07 +0530

    practice prototype and constructor function

---
+++ #16 object oriented programming/practice/script.js
@@ -0,0 +1,44 @@
+// the contructor function
+function Car(maker, color, model, highSpeed, accerlation, currentSpeed){
+    this.maker = maker;
+    this.color = color;
+    this.model = model;
+    this.highSpeed = highSpeed;
+    this.accerlation = accerlation;
+    this.currentSpeed = currentSpeed;
+}
+
+Car.prototype.stop = function (){
+    if (this.currentSpeed > 0) this.currentSpeed -= 30;
+    else this.currentSpeed = 0
+    return this.currentSpeed
+}
+
+Car.prototype.move = function(){
+        if (this.currentSpeed < this.highSpeed ) this.currentSpeed += this.accerlation;
+        else if (this.currentSpeed > this.highSpeed) this.currentSpeed = this.highSpeed;
+        return this.currentSpeed;
+}
+
+Car.prototype.description = function(){
+    return `${this.color} colored ${this.maker} ${this.model} is currently moving with the speed of ${this.currentSpeed}`
+}
+
+const lambo = new Car('Lamborghini', 'Lime-Yellow', 'Huracan', 360, 20, 40);
+console.log(lambo.stop());
+console.log(lambo.move());
+console.log(lambo.move());
+console.log(lambo.description())
+console.log(lambo.move());
+console.log(lambo.description())
+console.log(lambo.move());
+console.log(lambo.description())
+console.log(lambo.move());
+console.log(lambo.stop());
+console.log(lambo.description())
+console.log(lambo.stop());
+console.log(lambo.description())
+console.log(lambo.stop());
+console.log(lambo.description())
+console.log(lambo)
+
