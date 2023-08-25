5127fbe3b9c565a7c0a464dc524503414db571e5
gitanshu18 <lusifer0418@gmail.com>
2023-08-25 22:42:41 +0530

    learn about geolocation api

---
+++ #17 Geoloaction , Libs and Mapty project/script.js
@@ -0,0 +1,14 @@
+navigator.geolocation.getCurrentPosition(success, failed)
+
+function success(position){
+    let {latitude, longitude} = position.coords;
+    
+    console.log(position)
+    console.log(latitude, longitude)
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+}
+
+function failed(){
+    alert("Can't access position")
+}
