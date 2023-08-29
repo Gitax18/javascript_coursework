02362815ad6d1070e5f561fb59fa6a7d1c4249d8
gitanshu18 <lusifer0418@gmail.com>
2023-08-29 22:39:37 +0530

    apply OOPS architechture to project

--- #17 Geoloaction , Libs and Mapty project/geolocation.js
+++ #17 Geoloaction , Libs and Mapty project/geolocation.js
@@ -1,13 +1,47 @@
 'use strict';
 
+// const map container
+const mapContainer = document.querySelector('.map-container');
+
 navigator.geolocation.getCurrentPosition(success, failed)
 
 function success(position){
     let {latitude, longitude} = position.coords;
     
-    console.log(position)
     console.log(latitude, longitude)
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+    const map = L.map('map-container').setView([latitude, longitude], 12);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    // L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 300,
+            minWidth: 100,
+            className: 'marker-red',
+            autoClose: false,
+            closeOnClick: false,
+        }))
+        .setPopupContent("Your current location")
+        .openPopup();
+
+    map.on('click',(pos)=>{
+        const {lat, lng} = pos.latlng;
+        L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            maxWidth: 300,
+            minWidth: 100,
+            className: 'marker-red',
+            autoClose: false,
+            closeOnClick: false,
+        }))
+        .setPopupContent("hello")
+        .openPopup();
+    })
 
 }
 
--- #17 Geoloaction , Libs and Mapty project/project/script.js
+++ #17 Geoloaction , Libs and Mapty project/project/script.js
@@ -10,3 +10,96 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvt;
+
+class App{
+  #map;
+  #mapEvt;
+
+  constructor(){
+    this._getPosition();
+
+    form.addEventListener('submit', this._newWorkout.bind(this));
+
+    // chcnging input field on form according to the user workout.
+    inputType.addEventListener('change', this._toggleElevationField)
+  }
+
+  _getPosition(){
+    // running only if navigator exist 
+    if (navigator.geolocation) {
+      //   making navigator geolocation object
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), failed);
+      
+      // call back if the user rejects to give location 
+        function failed() {
+          alert("Can't access position");
+        }
+      }
+    }
+
+  _loadMap(position){
+    // retreiving coordinates for position object
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude]; // converting coords to array
+
+    // loading map on current coords with zoom (here Map zoom is 10)
+    this.#map = L.map('map').setView(coords, 10); 
+
+    // adding custom map theme to map, known as tile
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+   
+    /**
+    Adding event listener to map
+    We cannot use default addEventListener bcz it will add event to whole div in which
+    map is rendering but we only want to add click event on particular position on the   
+    map and for that we using leaflet built in map methong which 
+    is `.on(eventType, callback)`
+     */
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE){
+    this.#mapEvt = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus()
+  }
+
+  _toggleElevationField(){
+    inputElevation.closest('div').classList.toggle('form__row--hidden');
+    inputCadence.closest('div').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e){
+    e.preventDefault(); // preventing form reload
+    
+      const {lat,lng} = this.#mapEvt.latlng; // getting coords of clicked location
+    
+      L.marker([lat, lng]) // generating marker on clicked coordinates
+                .addTo(this.#map) // adding marker to map
+                .bindPopup(L.popup({ // custominzing popup (i.e. marker)
+                    maxWidth: 380,
+                    minWidth: 100,
+                    className: 'running-popup',
+                    autoClose: false,
+                    closeOnClick: false,
+                }))
+                .setPopupContent('🏃‍♀️Running🏃‍♂️') // setting inner HTML of the marker
+                .openPopup();
+    
+      // emptying all inputs
+      inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = "";
+      
+      // hiding form again after submission
+      this.classList.add('hidden').bind(form);
+  }
+}
+
+const app = new App();
+
+
+
+
