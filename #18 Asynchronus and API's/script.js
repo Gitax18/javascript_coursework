242d37501aff2c07d0a9b3f9744d1372ea5d0c62
gitanshu18 <lusifer0418@gmail.com>
2023-08-31 17:08:14 +0530

    started learning API's

---
+++ #18 Asynchronus and API's/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+// https://countries-api-836d.onrender.com/countries/
