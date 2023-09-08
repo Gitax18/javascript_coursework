f882afe26663119b4c77230a95915abd98d4425e
gitanshu18 <lusifer0418@gmail.com>
2023-09-08 21:29:14 +0530

    learn modern JS technique

--- #19 modules and toolings/clean.js
+++ #19 modules and toolings/clean.js
@@ -1,4 +1,4 @@
-var budget = [
+const budget = [
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -9,52 +9,42 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const expenseLimit = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const addExpense = function (value, description, user = "jonas") {
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+  let lim = expenseLimit[user] ? expenseLimit[user] : 0;
 
   if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
+    budget.push({ value: -value, description, user });
   }
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
+const checkExpense = function () {
+  for (const el of budget) {
+    let lim = expenseLimit[el.user] ? expenseLimit[el.user] : 0;
 
     if (el.value < -lim) {
       el.flag = 'limit';
     }
+    
   }
 };
-check();
+checkExpense();
 
 console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
+const bigExpenses = function (limit) {
+  const output = '';
+  for (const el of budget) {
     if (el.value <= -limit) {
       output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
     }
